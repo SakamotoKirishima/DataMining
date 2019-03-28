6c9f9f1f30ca0f15a113862095b567ac8343afbd
sakamotokirishima <mathangi.kuruta.sundar@gmail.com>
2019-03-28 15:56:22 +0530

    Completed code

--- Assignment2/KMeansClusturing.py
+++ Assignment2/KMeansClusturing.py
@@ -12,10 +12,13 @@ class KMeans:
     def __init__(self, df):
 
         self.dataframe = df
+        self.dataframe.dropna()
         self.y = df[' brand']
         self.y = pd.get_dummies(self.y)
         # self.y=self.y.as_matrix(columns=None)
         self.dataframe = self.preprocess(self.dataframe)
+        self.dataframe = pd.concat([self.dataframe, self.y], axis=1)
+        # self.dataframe.fillna(0)
         self.k = self.selectK(df)
         # self.centroids= dict()
         self.X = self.dataframe
@@ -54,30 +57,96 @@ class KMeans:
             # pprint(l)
             # for i in dimensions:
             #     print i
-            self.traceCentroid.append(go.Splom(dimensions=l, name= 'Centroid'+str(i),
-                                      marker=dict(color=[self.colorMap[i]], size=10, line=dict(width=0.5,
-                                                                                               color='rgb(230,230,230)'))))
+            self.traceCentroid.append(go.Splom(dimensions=l, name='Centroid' + str(i),
+                                               marker=dict(color=[self.colorMap[i]], size=10, line=dict(width=0.5,
+                                                                                                        color='rgb(255,0,0)'))))
         # exit()
 
     def plotUnClusteredGraph(self):
         l = list()
         for i in self.dataframe.columns:
             l.append(dict(label=i, values=self.dataframe[i].tolist()))
-        tracePoints = go.Splom(dimensions=l,name='Points',marker=dict(color=['rgb(230,230,230)'],size=10,line=dict(width=0.5,
-                                                                                               color='rgb(230,230,230)')))
+        tracePoints = go.Splom(dimensions=l, name='Points',
+                               marker=dict(color=['rgb(255,0,0)'], size=10, line=dict(width=0.5,
+                                                                                          color='rgb(255,0,0)')))
         data = [tracePoints]
         data.extend(self.traceCentroid)
         plot(data, filename='unclustered.html')
+
     def assign(self):
         # print self.dataframe.dtypes
         # exit()
-        self.dataframe= pd.concat([self.dataframe,self.y],axis=1)
+        # self.dataframe= pd.concat([self.dataframe,self.y],axis=1)
         for i in range(self.k):
-            self.dataframe['distance from centroid'+str(i)]=np.sqrt(np.square(self.dataframe[:]).sum(axis=1))
+            self.dataframe['distance from centroid{}'.format(i)] = 0
+            for j in range(len(self.dataframe)):
+                euclidianDistance = 0
+                for k in self.centroids['x'][i].index:
+                    # print self.dataframe[k][j],
+                    euclidianDistance += np.square(self.dataframe[k][j] - self.centroids['x'][i][k])
+                # print
+                euclidianDistance = np.sqrt(euclidianDistance)
+                # print euclidianDistance
+                self.dataframe['distance from centroid{}'.format(i)][j] = euclidianDistance
+        # print self.dataframe.head()
+        # exit()
         centroid_distance_cols = ['distance from centroid{}'.format(i) for i in range(self.k)]
         self.dataframe['closest'] = self.dataframe.loc[:, centroid_distance_cols].idxmin(axis=1)
-        self.dataframe['closest'] = self.dataframe['closest'].map(lambda x: int(x.lstrip('distance from centroid')))
-        print self.dataframe.head()
+        # self.dataframe['closest'] = self.dataframe['closest'].map(lambda x: int(x.lstrip('distance from centroid')))
+        self.dataframe['closest'] = self.dataframe.closest.str.replace('distance from centroid', '')
+        # self.dataframe['closest']= self.dataframe['closest'].astype(int)
+        # self.dataframe.dropna()
+        # self.dataframe= self.dataframe.drop(self.dataframe[(self.dataframe['closest'] != '1') & (self.dataframe['closest'] != '2') &(self.dataframe['closest'] != '0')].index)
+        # print self.dataframe.iloc[14]
+        # print self.dataframe.head()
+
+    def updateCentroids(self):
+        i = 0
+        # print type(self.centroids['x'][0])
+        for i in range(self.k):
+            meanCentroid = self.dataframe[self.dataframe['closest'] == str(i)].mean()
+            # print type(self.centroids['x'])
+            # print meanCentroid
+            for j in self.centroids['x'][i].index:
+                self.centroids['x'][i][j] = meanCentroid[j]
+            # print self.centroids['x'][i]
+
+            # print self.centroids['x'][0].index
+            # name = self.centroids['x'][0].index[0]
+            # print self.centroids['x'][0][name]
+            # print meanCentroid[name]
+
+    def cluster(self):
+        self.assign()
+        i = 0
+        while True:
+            closest_centroids = self.dataframe['closest'].copy(deep=True)
+            self.updateCentroids()
+            self.assign()
+            # print i
+            i += 1
+            if closest_centroids.equals(self.dataframe['closest']) or i > 30:
+                break
+
+    def plotClusteredGraph(self):
+        tracePoints = list()
+        i = 0
+        for i in range(self.k):
+            l = list()
+            dataframeForCentroid = self.dataframe[self.dataframe['closest'] == str(i)]
+            # print dataframeForCentroid.head()
+            # exit()
+            # print type(dataframeForCentroid)
+            for j in dataframeForCentroid.columns:
+                # print type(dataframeForCentroid[j])
+                l.append(dict(label=j, values=dataframeForCentroid[j]))
+            tracePoints.append(go.Splom(dimensions=l, name='Closest from centroid'+str(i),
+                                        marker=dict(color=[self.colorMap[int(i)]]* len(dataframeForCentroid.columns),
+                                                    size=10, line=dict(width=0.5, color=self.colorMap[int(i)]))))
+        data = tracePoints
+        data.extend(self.traceCentroid)
+        plot(data, filename='clustered.html')
+
     @staticmethod
     def selectK(dataframe):
         l = dataframe[' brand'].unique()
@@ -155,8 +224,9 @@ class KMeans:
 df = pd.read_csv('cars.csv')
 if __name__ == '__main__':
     o = KMeans(df)
-    # o.plotUnClusteredGraph()
-    # o.cluster()
-    o.assign()
+    o.plotUnClusteredGraph()
+    o.cluster()
+    o.plotClusteredGraph()
+    # o.assign()
     # o.plotAssigned()
     # o.updateCentroids()
